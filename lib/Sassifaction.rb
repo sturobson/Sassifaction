3520a87c12a7e32dd67c01a494e0bd4b7da12f77
Jed Foster <jed@jedfoster.com>
2014-03-22 10:22:52 -0700

    Add library file and Sassifaction SCSS partial

---
+++ lib/Sassifaction.rb
@@ -0,0 +1,6 @@
+require 'compass'
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+# stylesheets_dir = File.join(base_directory, 'my', 'stylesheets')
+
+Compass::Frameworks.register('stipe', :stylesheets_directory => File.join(extension_path, 'sass'), :path => extension_path)
+

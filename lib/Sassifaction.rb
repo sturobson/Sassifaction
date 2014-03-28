4eb13ae15fa8086af750866c4ec2e83e01762f2f
Jed Foster <jed@jedfoster.com>
2014-03-27 20:55:14 -0700

    Fix a bonehead mistake
    This is what happens when I cut and paste code.

--- lib/Sassifaction.rb
+++ lib/Sassifaction.rb
@@ -2,5 +2,5 @@ require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 # stylesheets_dir = File.join(base_directory, 'my', 'stylesheets')
 
-Compass::Frameworks.register('stipe', :stylesheets_directory => File.join(extension_path, 'sass'), :path => extension_path)
+Compass::Frameworks.register('Sassifaction', :stylesheets_directory => File.join(extension_path, 'sass'), :path => extension_path)
 
